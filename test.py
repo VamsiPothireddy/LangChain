8b98f676b898549d5e0f7ff85cf497c742b78310
Vamsi Pothireddy <47288461+VamsiPothireddy@users.noreply.github.com>
2025-10-13 17:54:50 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import langchain
 print ("hello")
 print (langchain.__version__)
+
+# alternate Hugging Face access token placeholder
+HUGGINGFACEHUB_API_TOKEN="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"
